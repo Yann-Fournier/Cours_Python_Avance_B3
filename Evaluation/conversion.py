956f9774c1c0837b4cb75dbc0754c7906f75922e
Yann Fournier <y.fourni@icloud.com>
2025-06-02 16:04:08 +0200

    fin évaluation

--- Evaluation/conversion.py
+++ Evaluation/conversion.py
@@ -30,7 +30,18 @@ def conversion_menu(conversions):
     Returns:
     int: Choix de l'utilisateur.
     """
-    pass
+    print("Menu des Convertions:")
+    cpt = 1
+    for unit1, unit2, _ in conversions:
+        print(f"{cpt}. {unit1} vers {unit2}")
+        print(f"{cpt + 1}. {unit2} vers {unit1}")
+        cpt += 2
+    
+    user_input = demander_valeur_numerique_utilisateur(1, len(conversions) * 2)
+    while user_input == "N/A":
+        user_input = demander_valeur_numerique_utilisateur(1, len(conversions) * 2)
+    return user_input
+    
 
 
 def demander_valeur_numerique_utilisateur(valeur_min, valeur_max):
@@ -44,7 +55,17 @@ def demander_valeur_numerique_utilisateur(valeur_min, valeur_max):
     Returns:
     int: Valeur saisie par l'utilisateur.
     """
-    pass
+    user_input = input("Donnez une valeur entre 1 et 6 : ")
+    try:
+        choix = int(user_input)
+        if valeur_min <= choix <= valeur_max:
+            return choix
+        else:
+            print("Choix invalide. Veuillez réessayer.")
+            return "N/A"
+    except ValueError:
+        print("Entrée invalide. Veuillez entrer un nombre.")
+        return "N/A"
 
 
 def demander_et_afficher_conversion(unit1, unit2, facteur, reverse=False):
@@ -60,18 +81,45 @@ def demander_et_afficher_conversion(unit1, unit2, facteur, reverse=False):
     Returns:
     bool: True si l'utilisateur souhaite quitter, False sinon.
     """
-    pass
+    if reverse:
+        user_input = input(f"Entrez la valeur en {unit2} à convertir en {unit1} (ou 'q' pour quitter): ")
+    else:
+        user_input = input(f"Entrez la valeur en {unit1} à convertir en {unit2} (ou 'q' pour quitter): ")
+    
+    if user_input.lower() == 'q':
+        print("Merci d'avoir utilisé le programme de conversion. Au revoir!")
+        return False
+    try:
+        valeur = float(user_input)
+        if reverse:
+            resultat = valeur / facteur
+            print(f"Résultat de la convertion : {valeur} {unit2} équivaut à {resultat:.2f} {unit1}")
+            return True
+        else:
+            resultat = valeur * facteur
+            print(f"Résultat de la convertion : {valeur} {unit1} équivaut à {resultat:.2f} {unit2}")
+            return True
+    except ValueError:
+        print("Entrée invalide. Veuillez entrer un nombre.")
+        return True
 
 # Menu principal
 def main():
     print("Ce programme vous permet d'effectuer des conversions d'unités")
 
     # Menu : choix de la conversion
-    # .........
+    choix = conversion_menu(CONVERSIONS)
     
 
     # Demander les valeurs à convertir à l'utilisateur
-    # .........
+    boucle_continue = True
+    while boucle_continue:
+        boucle_continue = demander_et_afficher_conversion(
+            CONVERSIONS[(choix - 1) // 2][0],
+            CONVERSIONS[(choix - 1) // 2][1],
+            CONVERSIONS[(choix - 1) // 2][2],
+            reverse=(choix % 2 == 0)
+        )
 
         
 if __name__ == "__main__":
